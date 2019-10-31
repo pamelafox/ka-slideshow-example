5f37e638ad7d7dcd37f45e674c139ca3b1d7bac7
Pamela Fox <pamela.fox@gmail.com>
2019-10-31 16:45:36 -0700

    Adding slideshow JS file

---
+++ slideshow.js
@@ -0,0 +1,29 @@
+/**
+ * This function takes in a reference to a DOM element,
+ * assumes each of its children are <img> tags,
+ * and then makes a simple slideshow out of the images.
+ * @param container A DOM element
+ */
+var slideShow = function(container) {
+    this.images = [];
+    this.curImage = 0;
+    for (i = 0; i < container.childElementCount; i++) {
+        this.images.push(container.children[i]);
+        this.images[i].style.display = "none";
+    }
+    
+    // Handle going to to the next slide
+    var nextSlide = function() {
+        for (var i = 0; i < this.images.length; i++) {
+            this.images[i].style.display = "none";
+        }
+        this.images[this.curImage].style.display = "block";
+        this.curImage++;
+        if (this.curImage >= this.images.length) {
+            this.curImage = 0;
+        }
+        window.setTimeout(nextSlide.bind(this), 500);
+    };
+    
+    nextSlide.call(this);
+};
